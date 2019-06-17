34cde00bf66a904ac3e59aa8a3c19b88ff020bd2
1893713 <kevalbhaimiyani5@gmail.com>
2019-06-17 14:55:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1893713/firebase/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a1893713.firebase;
+
+import android.support.v4.app.FragmentTransitionImpl;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FirebaseApp.initializeApp(this);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, ghpchu!");
+    }
+}
