89ea2edd84e5cd1c0ca91ad70adf2064b2a4ef3a
1893713 <kevalbhaimiyani5@gmail.com>
2019-06-17 16:14:52 -0400

    final data retrive fire base

---
+++ app/src/main/java/com/example/a1893713/firebase/Github.java
@@ -0,0 +1,30 @@
+package com.example.a1893713.firebase;
+
+public class Github {
+    int id,watchers;
+    String full_name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getWatchers() {
+        return watchers;
+    }
+
+    public void setWatcher(int watcher) {
+        this.watchers = watcher;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+}
--- app/src/main/java/com/example/a1893713/firebase/MainActivity.java
+++ app/src/main/java/com/example/a1893713/firebase/MainActivity.java
@@ -1,26 +1,67 @@
 package com.example.a1893713.firebase;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentTransitionImpl;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt_nm;
+    ArrayList<Github> vals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt_nm= findViewById(R.id.txt_nm);
 
+
+        vals = new ArrayList<>();
 
         FirebaseApp.initializeApp(this);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("items");
 
-        myRef.setValue("Hello, ghpch!");
+
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+              //  String nm = dataSnapshot.child("0").child("name").getValue().toString();
+
+                 // txt_nm.setText(nm);
+
+
+                  Iterable<DataSnapshot>childs = dataSnapshot.getChildren();
+                  for (DataSnapshot snap : childs){
+
+                   //   System.out.println("Name :"+snap.child("name").getValue().toString());
+
+                      Github git = snap.getValue(Github.class);
+                      System.out.println(git.getId()+" "+ git.getFull_name()+" "+git.getWatchers());
+                  }
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
